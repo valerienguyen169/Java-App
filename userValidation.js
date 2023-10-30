531c215efb2c4ab2fad76ced44199d8dbd90bf8d
Rishitha0 <98759037+Rishitha0@users.noreply.github.com>
2023-10-30 09:22:53 -0500

    Update userValidation.js

--- userValidation.js
+++ userValidation.js
@@ -1,22 +1,28 @@
 "use strict";
 
 const Joi = require('joi');
+function validateForm() {
+      const email = document.getElementById("email").value;
+      const password = document.getElementById("password").value;
+      const acceptTerms = document.getElementById("accept-terms").checked;
 
-const {makeBodyValidator} = require("./makeValidation");
+      if (email.trim() === "") {
+        alert("Please enter your email.");
+        return false;
+      }
 
-const userSchema = Joi.object({
-    "password": Joi.string()
-            .token()
-            .lowercase()
-            .required(),
-    "username": Joi.string()
-        .token()
-        .lowercase()
-        .required()
-}); 
+      if (password.trim() === "") {
+        alert("Please enter your password.");
+        return false;
+      }
 
-const userValidation = makeBodyValidator(userSchema);
+      if (!acceptTerms) {
+        alert("Please accept the terms and conditions.");
+        return false;
+      }
 
-module.exports = {
-    userValidation,
+      // If all validations pass, the form will be submitted
+      return true;
+    }
+
 }; 
